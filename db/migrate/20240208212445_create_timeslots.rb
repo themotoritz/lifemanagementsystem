d23dac1f3713bde6b84ac3463a3c08a67f9693af
emwf <emwf@linux.fritz.box>
2024-03-19 19:24:40 +0100

    increase possible size

--- db/migrate/20240208212445_create_timeslots.rb
+++ db/migrate/20240208212445_create_timeslots.rb
@@ -1,7 +1,7 @@
 class CreateTimeslots < ActiveRecord::Migration[7.1]
   def change
     create_table :timeslots do |t|
-      t.integer :size
+      t.bigint :size
       t.datetime :start_time
       t.datetime :end_time
       t.belongs_to :event, foreign_key: true
