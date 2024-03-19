b36b0b5094f159e300c450158c34f2b5feb31957
emwf <emwf@linux.fritz.box>
2024-03-19 19:24:52 +0100

    add attribute end_time

--- db/migrate/20240208205651_create_events.rb
+++ db/migrate/20240208205651_create_events.rb
@@ -3,6 +3,7 @@ class CreateEvents < ActiveRecord::Migration[7.1]
     create_table :events do |t|
       t.string :kind
       t.datetime :start_time
+      t.datetime :end_time
       t.integer :duration
       t.boolean :fixed
       t.text :description
