0c4a6839c8cd3fe83c72a512c418a8f383cc9baf
emwf <emwf@linux.fritz.box>
2024-03-19 21:44:29 +0100

    update scheduling function, destroy obsolete timeslots

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,32 +9,32 @@ class Event < ApplicationRecord
   validate :start_time_before_end_time
   validate :duration_positiv
   validate :no_overlapping_events_exist
-  validate :free_timeslot_available
   after_destroy :merge_surrounding_timeslots
+  after_commit :destroy_obsolete_timeslots
 
   private
 
   def start_time_before_end_time
     if start_time.present? && end_time.present? && start_time >= end_time
-      errors.add(:start_time, "start time must be prior to end time")
+      errors.add(:start_time, "must be prior to end time")
     end
   end
 
   def duration_positiv
     if duration.present? && duration < 0
-      errors.add(:start_time, "duration must not be negativ")
+      errors.add(:duration, "must not be negativ")
     end
   end
 
   def start_time_not_in_the_past
     if start_time.present? && start_time < Time.now
-      errors.add(:start_time, "start time must not be in the past")
+      errors.add(:start_time, "must not be in the past")
     end
   end
 
   def no_overlapping_events_exist
     if start_time.present?
-      overlapping_events = Event.where.not(id: id).where("start_time <= ? AND end_time >= ?", end_time, start_time)
+      overlapping_events = Event.where.not(id: id).where("start_time < ? AND end_time > ?", end_time, start_time)
       if overlapping_events.any?
         errors.add(:start_time, "is within another event's time frame")
         self.start_time = self.end_time = self.duration = nil
@@ -42,7 +42,23 @@ class Event < ApplicationRecord
     end
   end
 
-  def schedule 
+  def schedule
+    if Timeslot.where("start_time < ?", Time.now).where("end_time > ?", Time.now).count > 1
+      raise "FATAL: should not be possible"
+    end
+
+    current_timeslot =  Timeslot.where("start_time < ?", Time.now).where("end_time > ?", Time.now).first
+
+    if current_timeslot.present?
+      if current_timeslot.end_time > Time.now + 5.minutes
+        current_timeslot.update(start_time: Time.now + 5.minutes)
+      elsif current_timeslot.end_time <= Time.now + 5.minutes
+        current_timeslot.destroy
+      else
+        raise "FATAL: case not covered"
+      end
+    end
+
     timeslot = nil
 
     if start_time.nil? && end_time.nil? && duration.nil?
@@ -56,41 +72,46 @@ class Event < ApplicationRecord
       self.end_time = start_time + duration
     elsif start_time.present? && end_time.nil? && duration.nil?
       self.duration = 15.minutes
+      timeslot = find_free_timeslot
       self.end_time = start_time + duration
     elsif start_time.present? && end_time.nil? && duration.present?
+      timeslot = find_free_timeslot
       self.end_time = start_time + duration
-    elsif start_time.present? && end_time.present? && duration.nil?
-      self.duration = end_time - start_time
+    # elsif start_time.present? && end_time.present? && duration.nil?
+    #   self.duration = end_time - start_time
     end
-  end
-
-  def free_timeslot_available
-    timeslot = find_free_timeslot
 
     if timeslot.present?
       udpate_timeslots(timeslot)
     else
-      errors.add(:start_time, "Keinen freien Time Slot gefunden")
-      self.start_time = self.end_time = self.duration = nil
+      raise "FATAL: case not covered"
     end
   end
 
   def find_free_timeslot
-    if start_time.present?
-      Timeslot.order(:start_time).where("size > ?", duration).where("start_time <= ?", start_time).where("end_time >= ?", end_time).first
+    if start_time.present? 
+      timeslot = Timeslot.order(:start_time).where("size > ?", duration).where("start_time <= ?", start_time).where("end_time >= ?", end_time || start_time + duration).first
     elsif duration.present?
-      Timeslot.order(:start_time).where("size > ?", duration).where("start_time >= ?", Time.now).first
+      timeslot = Timeslot.order(:start_time).where("size > ?", duration).where("end_time >= ?", Time.now).first
     else
       raise "FATAL: case not covered"
     end
+
+    unless timeslot.present?
+      errors.add(:start_time, "Keinen freien Time Slot gefunden")
+      byebug
+      self.start_time = self.end_time = self.duration = nil
+    end
+
+    timeslot
   end
 
   def udpate_timeslots(timeslot)
     timeslot_start_time = timeslot.start_time
     timeslot_end_time = timeslot.end_time
 
-    timeslot.update(end_time: start_time-1.second, size: start_time-1.second - timeslot_start_time)
-    Timeslot.create(start_time: end_time+1.second, end_time: timeslot_end_time, size: timeslot_end_time - start_time+1.second)
+    timeslot.update(end_time: start_time, size: start_time - timeslot_start_time)
+    Timeslot.create!(start_time: end_time, end_time: timeslot_end_time, size: timeslot_end_time - start_time)
   end
 
   def merge_surrounding_timeslots
@@ -100,5 +121,10 @@ class Event < ApplicationRecord
     new_end_time = closest_subsequent_timeslot.end_time
     closest_subsequent_timeslot.destroy
     closest_previous_timeslot.update(end_time: new_end_time)
+  end
+
+  def destroy_obsolete_timeslots
+    Timeslot.where("start_time < ?", Time.now).where("end_time < ?", Time.now).destroy_all
+    Timeslot.where(size: 0).destroy_all
   end
 end
