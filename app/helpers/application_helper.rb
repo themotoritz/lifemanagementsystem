d146b085af57f4893131c275c00a9ab4e0b7d346
themotoritz <moritzfranke@hotmail.de>
2024-06-12 18:17:15 +0200

    add to params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,6 +34,8 @@ module ApplicationHelper
   def css_background_color_for(event)
     if event.recurrence_yearly?
       "bg-orange-300"
+    elsif event.fixed_date == true
+      "bg-red-300"
     else
       "bg-sky-300"
     end
